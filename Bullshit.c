fbfb504706a03bfe155c86483a8f8f75a4b882ea
UrosCvijanovic <59197547+UrosCvijanovic@users.noreply.github.com>
2022-04-06 21:15:09 +0800

    Update Bullshit.c

--- Bullshit.c
+++ Bullshit.c
@@ -6,7 +6,7 @@
 FILE *data_fp;
 static int block_size;
 
-void * printFunc(void * tid){
+void * search_start(void * tid){
     long * myID = (long *) tid;
     printf("Hello World, this is thread %ld\n", myID);
 }
@@ -19,12 +19,12 @@ int main() {
     pthread_t tid4;
     pthread_t tid5;
     pthread_t * pthreads[] = {&tid0, &tid1, &tid2, &tid3, &tid4, &tid5};
-    data_fp = fopen(“fileName.txt”, “r”);
-    int filesize = ftell(fp);
+    data_fp = fopen("test.txt", "r");
+    int filesize = ftell(data_fp);
     int n_threads = 6;
     block_size = filesize/n_threads;
     for(int i = 0; i < n_threads; i++){
-        start_offset = filesize*((float i)/n_threads) ;
+        int start_offset = filesize*(((float) i)/n_threads);
         pthread_create(pthreads[i], NULL, search_start, &start_offset);
     }
 
