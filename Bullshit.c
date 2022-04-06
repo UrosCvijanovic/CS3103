7557f33c27bd29a21cedc1912ffd8cf338b49064
UrosCvijanovic <59197547+UrosCvijanovic@users.noreply.github.com>
2022-04-06 21:51:35 +0800

    Update Bullshit.c

--- Bullshit.c
+++ Bullshit.c
@@ -15,13 +15,7 @@ int main() {
     pthread_t tid1;
     pthread_t tid2;
     pthread_t tid3;
-    pthread_t tid4;
-    pthread_t tid5;
-    pthread_t tid6;
-    pthread_t tid7;
-    pthread_t tid8;
-    pthread_t * pthreads[] = {&tid0, &tid1, &tid2, &tid3, &tid4, &tid5,
-    &tid6, &tid7, &tid8};
+    pthread_t * pthreads[] = {&tid0, &tid1, &tid2, &tid3};
     char line [128]; /* or some other suitable maximum line size */
 	char fileoutputname[15];
     int filecounter=1, linecounter=1;
@@ -31,14 +25,14 @@ int main() {
     if (!data_fp){
         return 1;
     }
-	
+
 	sprintf(fileoutputname, "file_part%d", filecounter);
 	ptr_writefile = fopen(fileoutputname, "w");
     
 
     while (fgets(line, sizeof line, data_fp)!=NULL) {
-		if (linecounter == 40) {
-            pthread_create(pthreads[filecounter], NULL, search_start(FILE **), &ptr_writefile);
+		if (linecounter == 80) {
+           		pthread_create(pthreads[filecounter], NULL, search_start(FILE **), &ptr_writefile);
 			fclose(ptr_writefile);
 			linecounter = 1;
 			filecounter++;
