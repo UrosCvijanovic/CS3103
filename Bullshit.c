53cef23f538fec0a4241b862109789ae5bbe6d3b
UrosCvijanovic <59197547+UrosCvijanovic@users.noreply.github.com>
2022-04-06 21:47:44 +0800

    Update Bullshit.c

--- Bullshit.c
+++ Bullshit.c
@@ -3,33 +3,57 @@
 #include <unistd.h>  //Header file for sleep(). man 3 sleep for details.
 #include <pthread.h>
 
-FILE *data_fp;
-static int block_size;
 
-void * search_start(void * tid){
-    long * myID = (long *) tid;
-    printf("Hello World, this is thread %ld\n", myID);
+void * search_start(FILE **fp){
+    
 }
 
 int main() {
+    FILE *data_fp;
+    FILE *ptr_writefile;
     pthread_t tid0;
     pthread_t tid1;
     pthread_t tid2;
     pthread_t tid3;
     pthread_t tid4;
     pthread_t tid5;
-    pthread_t * pthreads[] = {&tid0, &tid1, &tid2, &tid3, &tid4, &tid5};
+    pthread_t tid6;
+    pthread_t tid7;
+    pthread_t tid8;
+    pthread_t * pthreads[] = {&tid0, &tid1, &tid2, &tid3, &tid4, &tid5,
+    &tid6, &tid7, &tid8};
+    char line [128]; /* or some other suitable maximum line size */
+	char fileoutputname[15];
+    int filecounter=1, linecounter=1;
+
     data_fp = fopen("test.txt", "r");
-    int filesize = ftell(data_fp);
-    int n_threads = 6;
-    block_size = filesize/n_threads;
-    for(int i = 0; i < n_threads; i++){
-        int start_offset = filesize*(((float) i)/n_threads);
-        // need to make smaller files first
-        pthread_create(pthreads[i], NULL, search_start, //here we need to add segment, i guess...);
+
+    if (!data_fp){
+        return 1;
     }
-
-
+	
+	sprintf(fileoutputname, "file_part%d", filecounter);
+	ptr_writefile = fopen(fileoutputname, "w");
     
+
+    while (fgets(line, sizeof line, data_fp)!=NULL) {
+		if (linecounter == 40) {
+            pthread_create(pthreads[filecounter], NULL, search_start(FILE **), &ptr_writefile);
+			fclose(ptr_writefile);
+			linecounter = 1;
+			filecounter++;
+			sprintf(fileoutputname, "file_part%d", filecounter);
+			ptr_writefile = fopen(fileoutputname, "w");
+			if (!data_fp){
+                return 1;
+            }	
+		}
+		fprintf(ptr_writefile,"%s\n", line);
+		linecounter++;
+	}
+
+	fclose(data_fp);
+
     return 0;
 }
+   
